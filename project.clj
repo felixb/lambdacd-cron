09775792784b73c188996a09e494e3251a48b288
Felix Bechstein <f@ub0r.de>
2016-04-19 18:45:02 +0200

    Prepare next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdacd-cron "0.1.0"
+(defproject lambdacd-cron "0.1.1-SNAPSHOT"
   :description "A cron for your lambdacd"
   :url "https://github.com/felixb/lambdacd-cron"
   :license {:name "Apache License, version 2.0"

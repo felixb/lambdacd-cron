997bfda9e4f740195a0627d2114e94b692ccf687
Felix Bechstein <f@ub0r.de>
2016-04-20 20:50:33 +0200

    Show step as :waiting while waiting for cron pattern to match

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## 0.1.1
+
+* Show step as ```:waiting``` while waiting for cron pattern to match
+
 ## 0.1.0
 
 * initial release
--- src/lambdacd_cron/core.clj
+++ src/lambdacd_cron/core.clj
@@ -1,10 +1,17 @@
 (ns lambdacd-cron.core
   (:require [lambdacd.steps.support :as support]
-            [clj-time.core :as t]))
+            [clj-time.core :as t]
+            [clojure.core.async :as async]))
 
 (defn- is-wildcard [target]
   (or (= "*" target)
       (nil? target)))
+
+(defn- nil-to-star [x]
+  (if (nil? x) "*" (str x)))
+
+(defn- pattern-to-str [pattern]
+  (clojure.string/join " " (map nil-to-star pattern)))
 
 (defn matches
   "Check if a date matches the given cron pattern."
@@ -32,5 +39,8 @@
   "Build step that waits for a default cron pattern to match"
   [& [minutes hours day-of-month month day-of-week]]
   (fn [_ ctx]
-    (let [wait-result (wait-for-cron-while-not-killed ctx minutes hours day-of-month month day-of-week)]
+    (let [result-ch (:result-channel ctx)
+          _ (async/>!! result-ch [:status :waiting])
+          _ (async/>!! result-ch [:out (str "Waiting for cron with pattern " (pattern-to-str [minutes hours day-of-month month day-of-week]) " ...")])
+          wait-result (wait-for-cron-while-not-killed ctx minutes hours day-of-month month day-of-week)]
       wait-result)))
